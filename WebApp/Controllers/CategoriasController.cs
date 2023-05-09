9d93c6f57bb7c68c8570ddec69738b81bfb437eb
Heron Emanuel Silva dos Santos <heron.santos@academico.ifrn.edu.br>
2023-05-09 15:38:55 -0300

    Adequar Categorias

--- WebApp/Controllers/CategoriasController.cs
+++ WebApp/Controllers/CategoriasController.cs
@@ -2,26 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Net;
 using System.Web.Mvc;
 using WebApp.Models;
+using WebApp.Context;
+using System.Data.Entity;
 
 namespace WebApp.Controllers
 {
     public class CategoriasController : Controller
     {
-        private static IList<Categoria> categorias = new List<Categoria>()
-        {
-            new Categoria() { CategoriaId = 1, Nome = "Notebooks"},
-            new Categoria() { CategoriaId = 2, Nome = "Monitores"},
-            new Categoria() { CategoriaId = 3, Nome = "Impressoras"},
-            new Categoria() { CategoriaId = 4, Nome = "Mouses"},
-            new Categoria() { CategoriaId = 5, Nome = "Desktops"}
-        };
-
+        private readonly EFContext context = new EFContext();
         // GET: Categorias
         public ActionResult Index()
         {
-            return View(categorias);
+            return View(context.Categorias.OrderBy(c => c.Nome));
         }
         
         public ActionResult Create()
@@ -33,42 +28,82 @@ namespace WebApp.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create(Categoria categoria)
         {
-            categorias.Add(categoria);
-            categoria.CategoriaId = categorias.Select(m => m.CategoriaId).Max() + 1;
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            context.Categorias.Add(categoria);
+            context.SaveChanges();
             return RedirectToAction("Index");
         }
 
-        public ActionResult Edit(long id)
+        public ActionResult Edit(long? id)
         {
-            return View(categorias.Where(x => x.CategoriaId == id).First());
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Categoria c = context.Categorias.Find(id);
+            if (c == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(c);
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Edit(Categoria categoria)
         {
-            categorias.Remove(
-            categorias.Where(c => c.CategoriaId == categoria.CategoriaId).First());
-            categorias.Add(categoria);
+            if (!ModelState.IsValid)
+            {
+                return View(categoria);
+            }
+
+            context.Entry(categoria).State = EntityState.Modified;
+            context.SaveChanges();
             return RedirectToAction("Index");
         }
 
-        public ActionResult Details(long id)
+        public ActionResult Details(long? id)
         {
-            return View(categorias.Where(m => m.CategoriaId == id).First());
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Categoria c = context.Categorias.Find(id);
+            if (c == null)
+            {
+                return HttpNotFound();
+            }
+            return View(c);
         }
 
-        public ActionResult Delete(long id)
+        public ActionResult Delete(long? id)
         {
-            return View(categorias.Where(m => m.CategoriaId == id).First());
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Categoria c = context.Categorias.Find(id);
+            if (c == null)
+            {
+                return HttpNotFound();
+            }
+            return View(c);
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(Categoria categoria)
+        public ActionResult Delete(long id)
         {
-            categorias.Remove(
-            categorias.Where(c => c.CategoriaId == categoria.CategoriaId).First());
+            Categoria c= context.Categorias.Find(id);
+            context.Categorias.Remove(c);
+            context.SaveChanges();
+            TempData["message"] = $"Categoria {c.Nome} foi removida com sucesso";
             return RedirectToAction("Index");
         }
     }
